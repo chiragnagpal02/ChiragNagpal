5895b859207f682991c38a75ab7f36a0fc157009
Chirag Nagpal <85030123+chiragnagpal02@users.noreply.github.com>
2023-08-24 22:10:53 +0800

    change link to

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,7 +35,7 @@ const Navbar = () => {
     >
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
         <Link
-          to='/'
+          to='https://chiragnagpal02.github.io/ChiragNagpal/'
           className='flex items-center gap-2'
           onClick={() => {
             setActive("");
